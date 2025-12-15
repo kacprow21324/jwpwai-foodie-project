e2ec94fed75138647f37deed9ff78ea1678a8297
kacprow21324 <kawoszczylo.work@gmail.com>
2025-12-15 11:35:22 +0100

    Add meal sharing form and not-found page
    Introduced a new ShareMealPage component with a styled form for submitting meal details, using a new CSS module for layout and appearance. Added a not-found page for invalid meal routes.

---
+++ app/meals/[someName]/not-found.jsx
@@ -0,0 +1,10 @@
+"use client";
+import React from 'react';
+import './../../globals.css'
+
+export default function NotFound() {
+    return <main className="not-found">
+        <h1>This food is not found.</h1>
+        <p>The page you are looking for does not exist.</p>
+    </main>;
+}
