db3d70854933776d03b4fe9b6fe3d78e3a76cc94
kacprow21324 <kawoszczylo.work@gmail.com>
2025-12-26 21:08:28 +0100

    Add image upload for meal sharing
    Introduced an ImagePicker component and related styles for selecting images when sharing a meal. Implemented server-side logic to handle image uploads and save meal data, updating the share meal page to use the new image picker and form action. Updated database and added sample images.

---
+++ app/lib/actions.js
@@ -0,0 +1,24 @@
+'use server';
+
+import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+import { saveMeal } from './meals';
+
+function isInvalidText(text) {
+  return !text || text.trim() === '';
+}
+
+export async function shareMeal(formData) {
+  const meal = {
+    title: formData.get('title'),
+    summary: formData.get('summary'),
+    instructions: formData.get('instructions'),
+    image: formData.get('image'),
+    creator: formData.get('name'),
+    creator_email: formData.get('email'),
+  };
+
+  await saveMeal(meal);
+  revalidatePath('/meals');
+  redirect('/meals');
+}
--- app/lib/meals.js
+++ app/lib/meals.js
@@ -1,3 +1,4 @@
+import fs from 'node:fs';
 import sql from 'better-sqlite3';
 import path from 'path';
 
@@ -6,10 +7,53 @@ const db = sql(dbPath);
 
 export async function getMeals() {
     await new Promise((resolve) => setTimeout(resolve, 2000));
-    //throw new Error("Simulated database error");
-    return db.prepare(`SELECT * FROM meals`).all(); 
+    // throw new Error("Simulated database error");
+    return db.prepare(`SELECT * FROM meals`).all();
 }
 
-export function getMeal(slug){
+export function getMeal(slug) {
     return db.prepare(`SELECT * FROM meals WHERE slug = ?`).get(slug);
+}
+
+export async function saveMeal(meal) {
+    meal.slug = meal.title
+        .toLowerCase()
+        .trim()
+        .replace(/[^\w\s-]/g, '')
+        .replace(/[\s_-]+/g, '-')
+        .replace(/^-+|-+$/g, '');
+
+    const extension = meal.image.name.split('.').pop();
+    const fileName = `${meal.slug}.${extension}`;
+    const stream = fs.createWriteStream(`public/images/${fileName}`);
+    const bufferedImage = await meal.image.arrayBuffer();
+
+    stream.write(Buffer.from(bufferedImage), (error) => {
+        if (error) {
+            throw new Error('Saving image failed!');
+        }
+    });
+
+    meal.image = `/images/${fileName}`;
+    const instructionsClean = meal.instructions.replace(/<[^>]*>?/gm, '');
+
+    const stmt = db.prepare(`
+        INSERT INTO meals 
+        (title, summary, instructions, creator, creator_email, image, slug)
+        VALUES (
+            @title,
+            @summary,
+            @instructions,
+            @creator,
+            @creator_email,
+            @image,
+            @slug
+        )
+    `);
+
+   const info = stmt.run({
+        ...meal,
+        instructions: instructionsClean
+    });
+
 }
